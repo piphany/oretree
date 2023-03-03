371fc6b91566fea04f83eba49e268980063a0174
piphany <1078229683@qq.com>
2023-03-03 11:31:39 +0800

    MCMOD

---
+++ src/main/java/net/piphany/oretree/OreTree.java
@@ -0,0 +1,20 @@
+package net.piphany.oretree;
+
+import net.fabricmc.api.ModInitializer;
+//import net.piphany.oretree.block.ModBlocks;
+//import net.piphany.oretree.item.ModItemGroup;
+//import net.piphany.oretree.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OreTree implements ModInitializer {
+	public static final String MOD_ID = "oretree";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	//	ModItemGroup.registerItemGroups();
+	//	ModBlocks.registerModBlocks();
+	//	ModItems.registerModItems();
+	}
+}
